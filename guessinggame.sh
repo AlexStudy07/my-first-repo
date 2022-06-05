e2cb1b7498b55f7b0091bb453b019f070615be73
alexandru <alexandrusandu93@gmail.com>
2022-06-05 23:35:55 +0300

    final commit - 10

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,10 @@
+#declare function
 function fn {
 echo "How much files there are in this dir?"
+#using read for data imput
 read nrfiles
 actnrfiles=$(ls | wc -l)
-
+#IF statement
 if [[ acnrfiles -gt nrfiles ]]
    then 
      echo "actual nr of files is greather"
@@ -11,12 +13,12 @@ elif [[ acnrfiles -lt nrfiles ]]
      echo "actual nr of files is lower"
 fi
 }
-
+#call function
 fn
-
+#while loop
 while [[ ! nrfiles -eq actnrfiles ]]
 do
    fn
 done
-
+#congrats if success
 echo "Congrs the nr of files is right"
