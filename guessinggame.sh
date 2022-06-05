ed42b36509697cf33c690f6de1c04acc89084bb7
alexandru <alexandrusandu93@gmail.com>
2022-06-05 14:46:29 +0300

    first commit

---
+++ guessinggame.sh
@@ -0,0 +1,18 @@
+function fn {
+echo "How much files there are in this dir?"
+read nrfiles
+actnrfiles=$(ls | wc -l)
+if [[ acnrfiles -gt nrfiles ]]
+   then 
+     echo "actual nr of files is greather"
+elif [[ acnrfiles -lt nrfiles ]] 
+   then
+     echo "actual nr of files is lower"
+fi
+}
+fn
+while [[ ! nrfiles -eq actnrfiles ]]
+do
+   fn
+done
+echo "Congrs the nr of files is right"
